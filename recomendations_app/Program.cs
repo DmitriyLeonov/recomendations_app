d5b42e5bca3b1b21daf71c2f6a09e95804df0f6b
DmitriyLeonov <dleonov701@gmail.com>
2023-04-08 10:39:49 +0300

    facebook login fixed

--- recomendations_app/Program.cs
+++ recomendations_app/Program.cs
@@ -13,7 +13,7 @@ namespace Recomendations_app
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            // Add services to the container.
+            
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -53,7 +53,7 @@ namespace Recomendations_app
                     context.Response.Redirect(https);
                 }
             });
-            // Configure the HTTP request pipeline.
+            
             if (app.Environment.IsDevelopment())
             {
                 app.UseMigrationsEndPoint();
