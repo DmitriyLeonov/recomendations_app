a11be57e0cf784663178a2ed8513fcabb5b4b688
DmitriyLeonov <dleonov701@gmail.com>
2023-04-08 15:16:28 +0300

    rewiew controller and views

---
+++ recomendations_app/Controllers/ReviewController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Recomendations_app.Data;
+using Recomendations_app.Models;
+
+namespace Recomendations_app.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Review
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Reviews.Include(r => r.Author).Include(r => r.Subject);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Review/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var reviewModel = await _context.Reviews
+                .Include(r => r.Author)
+                .Include(r => r.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(reviewModel);
+        }
+
+        // GET: Review/Create
+        public IActionResult Create()
+        {
+            ViewData["AuthorId"] = new SelectList(_context.Set<UserModel>(), "Id", "Id");
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name");
+            return View();
+        }
+
+        // POST: Review/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ReviewCategory,AuthorGrade,ReviewBody,DateOfCreationInUTC,SubjectId,ReviewImageId,ImageLink,AuthorId")] ReviewModel reviewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(reviewModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Set<UserModel>(), "Id", "Id", reviewModel.AuthorId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", reviewModel.SubjectId);
+            return View(reviewModel);
+        }
+
+        // GET: Review/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var reviewModel = await _context.Reviews.FindAsync(id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Set<UserModel>(), "Id", "Id", reviewModel.AuthorId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", reviewModel.SubjectId);
+            return View(reviewModel);
+        }
+
+        // POST: Review/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Title,ReviewCategory,AuthorGrade,ReviewBody,DateOfCreationInUTC,SubjectId,ReviewImageId,ImageLink,AuthorId")] ReviewModel reviewModel)
+        {
+            if (id != reviewModel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reviewModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReviewModelExists(reviewModel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["AuthorId"] = new SelectList(_context.Set<UserModel>(), "Id", "Id", reviewModel.AuthorId);
+            ViewData["SubjectId"] = new SelectList(_context.Subjects, "Id", "Name", reviewModel.SubjectId);
+            return View(reviewModel);
+        }
+
+        // GET: Review/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.Reviews == null)
+            {
+                return NotFound();
+            }
+
+            var reviewModel = await _context.Reviews
+                .Include(r => r.Author)
+                .Include(r => r.Subject)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reviewModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(reviewModel);
+        }
+
+        // POST: Review/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.Reviews == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Reviews'  is null.");
+            }
+            var reviewModel = await _context.Reviews.FindAsync(id);
+            if (reviewModel != null)
+            {
+                _context.Reviews.Remove(reviewModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReviewModelExists(string id)
+        {
+          return (_context.Reviews?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ recomendations_app/Models/ReviewViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Recomendations_app.Models
+{
+    public class ReviewViewModel
+    {
+        List<ReviewModel> Reviews { get; set; }
+        List<Comment> Comments { get; set; }
+    }
+}
